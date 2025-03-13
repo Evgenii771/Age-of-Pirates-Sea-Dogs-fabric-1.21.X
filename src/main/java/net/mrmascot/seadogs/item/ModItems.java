f898f239a3d00c961dc7daf1d44247380caa1874
pol <pol@g.com>
2025-03-13 19:55:54 -0300

    test commit1

--- src/main/java/net/mrmascot/seadogs/item/ModItems.java
+++ src/main/java/net/mrmascot/seadogs/item/ModItems.java
@@ -18,8 +18,7 @@ public class ModItems {
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(AgeOfPirates.MOD_ID, name), item);
-        //test1 pvd test1
-        //LLL LLL LLL
+        //test1 pvd
     }
 
     public static void registerModItems() {
