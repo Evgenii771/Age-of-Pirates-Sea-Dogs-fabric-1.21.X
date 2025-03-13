fd78c8406a1feec93ee0348f7766f5c40a377e28
Evgenii771 <druzhkovep@gmail.com>
2025-03-14 01:58:30 +0300

    commit1

--- src/main/java/net/mrmascot/seadogs/item/ModItems.java
+++ src/main/java/net/mrmascot/seadogs/item/ModItems.java
@@ -19,7 +19,6 @@ public class ModItems {
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(AgeOfPirates.MOD_ID, name), item);
         //test1 pvd
-        //test2 pvd
     }
 
     public static void registerModItems() {
